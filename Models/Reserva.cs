baf2dd6940f00fee7825d720c1c373a06b6141c8
42SternEric <stern.eric99@gmail.com>
2023-03-02 21:03:37 -0300

    Primeira versão do desafio intermediario Pottencial DIO

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,16 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTE AQUI*  OK!!!!!!!!!!!!!
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTE AQUI*   OK!!!!!!!!!
+                throw new Exception("Capacidade menor do que o número de hóspedes");
             }
         }
 
@@ -34,24 +35,24 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public int ObterQuantidadeHospedes()
-        {
+        {   
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *IMPLEMENTE AQUI*  OK!!!!!!!!!
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // *IMPLEMENTE AQUI* OK!!!!!!!!!
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
             if (true)
             {
-                valor = 0;
+                valor = (DiasReservados * Suite.ValorDiaria - ((DiasReservados * Suite.ValorDiaria) / 10));
             }
 
             return valor;
